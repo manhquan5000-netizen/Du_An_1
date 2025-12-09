81b3a173559de80a4066cc0755ec77105d2c251f
Hai Nam <hain64849@gmail.com>
2025-12-09 14:34:38 +0700

    admin

--- BanGiay_Api/models/Cart.js
+++ BanGiay_Api/models/Cart.js
@@ -6,10 +6,26 @@ const CartItemSchema = new mongoose.Schema({
     ref: "Product",
     required: true,
   },
-  so_luong: { type: Number, required: true, min: 1 },
-  kich_thuoc: { type: String, required: true },
-  gia: { type: Number, required: true }, // Giá tại thời điểm thêm vào giỏ
-});
+  so_luong: { 
+    type: Number, 
+    required: true, 
+    min: 1,
+    validate: {
+      validator: Number.isInteger,
+      message: "Số lượng phải là số nguyên"
+    }
+  },
+  kich_thuoc: { 
+    type: String, 
+    required: true,
+    trim: true
+  },
+  gia: { 
+    type: Number, 
+    required: true,
+    min: 0
+  }, // Giá tại thời điểm thêm vào giỏ
+}, { _id: true });
 
 const CartSchema = new mongoose.Schema(
   {
@@ -18,14 +34,32 @@ const CartSchema = new mongoose.Schema(
       ref: "User",
       required: true,
       unique: true,
+      index: true,
     },
-    items: [CartItemSchema],
+    items: {
+      type: [CartItemSchema],
+      default: []
+    },
   },
-  { timestamps: true }
+  { 
+    timestamps: true,
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
+  }
 );
 
+// Virtual để tính tổng tiền
+CartSchema.virtual("tong_tien").get(function() {
+  return this.items.reduce((total, item) => {
+    return total + (item.gia * item.so_luong);
+  }, 0);
+});
+
+// Virtual để đếm tổng số lượng sản phẩm
+CartSchema.virtual("tong_so_luong").get(function() {
+  return this.items.reduce((total, item) => {
+    return total + item.so_luong;
+  }, 0);
+});
+
 module.exports = mongoose.model("Cart", CartSchema);
-
-
-
-
